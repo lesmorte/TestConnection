ac0087818aa1ba5fcc788f4650d6f84b81714c5e
Keith Turnbull <lesmorte@hotmail.com>
2016-10-20 17:34:22 -0400

    Added Test Suite

---
+++ TestConnection/TestConnectionUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestConnectionUnitTest
+{
+    [TestClass]
+    public class BaseTests
+    {
+        [TestMethod]
+        public void FailTest()
+        {
+            Assert.IsTrue(false, "This test does NOT pass, ever!");
+        }
+
+        [TestMethod]
+        public void SuccessTest()
+        {
+            Assert.IsTrue(true, "This test does not FAIL, ever!");
+        }
+    }
+}
